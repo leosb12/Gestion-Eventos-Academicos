2ffe60112eff1a140409daeb2b80fa519e5134e7
Daniel F.Banegas <danielfbanegas@gmail.com>
2025-05-06 14:20:13 -0400

    Arreglar el componente Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./Components/Navbar.jsx";
 import Carousel from "./Components/Carousel.jsx";
 import Events from "./Components/Events.jsx";
 import TypewriterComponent from "typewriter-effect";
+import './App.css';
+import './index.css'
 
 const App = () => {
     return (
--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -5,13 +5,13 @@ const Carousel = () => {
         <div id="carouselExample" className="carousel slide" data-bs-ride="carousel" style={{ maxWidth: '100%', margin: '0 auto' }}>
             <div className="carousel-inner">
                 <div className="carousel-item active" data-bs-interval="3000">
-                    <img src="/sopenco.jpg" className="d-block w-100" alt="kiko1" style={{ height: 'auto', maxHeight: '39rem', objectFit: 'contain', objectPosition: 'center', backgroundColor: 'rgba(30, 30, 47, 0.8)' }} />
+                    <img src="/sopenco.jpg" className="d-block w-100 carousel-inner" alt="kiko1"/>
                 </div>
                 <div className="carousel-item" data-bs-interval="3000">
-                    <img src="/sisisis.jpg" className="d-block w-100" alt="kiko2" style={{ height: 'auto', maxHeight: '39rem', objectFit: 'contain', objectPosition: 'center', backgroundColor: 'rgba(30, 30, 47, 0.8)' }} />
+                    <img src="/sisisis.jpg" className="d-block w-100 carousel-inner" alt="kiko2"/>
                 </div>
                 <div className="carousel-item" data-bs-interval="3000">
-                    <img src="/Imagen%20de%20WhatsApp%202025-04-27%20a%20las%2013.36.12_be9d8d90.jpg" className="d-block w-100" alt="kiko3" style={{ height: 'auto', maxHeight: '39rem', objectFit: 'contain', objectPosition: 'center', backgroundColor: 'rgba(30, 30, 47, 0.8)' }} />
+                    <img src="/Imagen%20de%20WhatsApp%202025-04-27%20a%20las%2013.36.12_be9d8d90.jpg" className="d-block w-100 carousel-inner" alt="kiko3"/>
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
