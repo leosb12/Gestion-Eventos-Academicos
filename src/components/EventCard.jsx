549be0dcd3f5ae0c470a64492330c8ca923970e6
Daniel F.Banegas <danielfbanegas@gmail.com>
2025-05-14 02:57:22 -0400

    implementar crear eventos

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 const EventCard = ({ evento }) => (
   <div className="col">
     <div className="card h-100 shadow-sm">
-      {evento.imagen_url && (
         <img
-          src={evento.imagen_url}
+          src={evento.imagen_url || '/kiko.jpg'}
           className="card-img-top"
           alt={evento.nombre}
           style={{ objectFit: 'cover', height: '200px' }}
         />
-      )}
       <div className="card-body d-flex flex-column">
         <h5 className="card-title">{evento.nombre}</h5>
       </div>
