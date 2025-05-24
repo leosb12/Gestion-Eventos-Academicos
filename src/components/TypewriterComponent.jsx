ba0db0e68f0054648883464544e0d6bcfdb868c1
Your Name <leonardoserrate@gmail.com>
2025-05-24 16:43:59 -0400

    Cambios

--- src/components/TypewriterComponent.jsx
+++ src/components/TypewriterComponent.jsx
@@ -1,26 +1,45 @@
-import { useEffect, useRef } from 'react';
+import {useEffect, useRef} from 'react';
 import Typewriter from 'typewriter-effect/dist/core';
 
 const TypewriterText = () => {
-  const typewriterRef = useRef(null);
+    const typewriterRef = useRef(null);
 
-  useEffect(() => {
-    const typewriter = new Typewriter(typewriterRef.current, {
-      loop: true,
-      delay: 75,
-    });
+    useEffect(() => {
+        const typewriter = new Typewriter(typewriterRef.current, {
+            loop: true,
+            delay: 75,
+        });
 
-    typewriter
-      .typeString('¡No te Pierdas los Próximos Eventos de la Facultad!')
-      .pauseFor(200)
-      .start();
-  }, []);
+        typewriter
+            .typeString('¡No te Pierdas los Próximos\nEventos de la Facultad!')
+            .pauseFor(2000)
+            .deleteAll()
+            .start();
+    }, []);
 
-  return (
-    <section className="container-fluid w-50 mx-auto text-center pt-5 mb-sm-1 mb-lg-3" id="intro">
-      <h1 className="p-3 fs-1 border-top border-3" ref={typewriterRef}></h1>
-    </section>
-  );
+    return (
+        <section
+            className="container-fluid w-100 mx-auto text-center pt-5 mb-3"
+            id="intro"
+            style={{
+                position: 'relative',
+                minHeight: '7.5rem', // Espacio suficiente para texto en móviles
+                overflow: 'hidden',
+            }}
+        >
+            <h1
+                className="p-3 fs-1 fs-sm-2 fs-md-1 border-top border-3"
+                ref={typewriterRef}
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    margin: 'auto',
+                }}
+            ></h1>
+        </section>
+    );
 };
 
-export default TypewriterText;;
+export default TypewriterText;
